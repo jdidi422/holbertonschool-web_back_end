ceae1d8f6316dbaf6133c7acdd83e394d7e1a549
jdidi422 <rassiljdidi3@gmail.com>
2025-08-20 17:08:10 +0200

    chnage

--- ES6_basic/0-constants.js
+++ ES6_basic/0-constants.js
@@ -1,5 +1,11 @@
 
+
+
 export function taskFirst() {
+
+
+
+
   const task = 'I prefer const when I can.';
   return task;
 }
@@ -8,16 +14,18 @@ export function getLast() {
   return ' is okay';
 }
 
+
 export function taskNext() {
 
 
-  
+
   let combination = 'But sometimes let';
   combination += getLast();
 
   return combination;
 
-
-
+  
+  
 
 }
+
