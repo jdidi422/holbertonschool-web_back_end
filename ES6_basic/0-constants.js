440c276666c54289c46ced80f0a1729b21e614da
jdidi422 <rassiljdidi3@gmail.com>
2025-08-20 16:42:50 +0200

    chnage

--- ES6_basic/0-constants.js
+++ ES6_basic/0-constants.js
@@ -1,15 +1,17 @@
+// Use const for taskFirst variables
 export function taskFirst() {
-  var task = 'I prefer const when I can.';
+  const task = 'I prefer const when I can.';
   return task;
 }
 
+// No change needed for getLast
 export function getLast() {
   return ' is okay';
 }
 
+// Use let for taskNext variables
 export function taskNext() {
-  var combination = 'But sometimes let';
+  let combination = 'But sometimes let';
   combination += getLast();
-
   return combination;
 }
