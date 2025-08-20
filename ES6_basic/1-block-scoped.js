5fc307aa031ee570c36311b789a7267867dc5d2d
jdidi422 <rassiljdidi3@gmail.com>
2025-08-20 17:19:56 +0200

    chnage

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -1,6 +1,13 @@
-export default function taskBlock() {
+export default function taskBlock(trueOrFalse) {
   const task = false;
   const task2 = true;
+
+  // Les variables du bloc ne sont pas nécessaires, on garde le scope propre
+  if (trueOrFalse) {
+    // Block-scoped, mais inutilisé pour ne pas affecter l'extérieur
+    const _task = true;
+    const _task2 = false;
+  }
 
   return [task, task2];
 }
