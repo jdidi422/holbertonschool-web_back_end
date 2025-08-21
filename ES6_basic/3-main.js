0b79b31e9b44743686156f5f0fbee312ce7b57d8
jdidi422 <rassiljdidi3@gmail.com>
2025-08-21 10:31:44 +0200

    chnage

---
+++ ES6_basic/3-main.js
@@ -0,0 +1,5 @@
+import getSumOfHoods from './3-default-parameter.js';
+
+console.log(getSumOfHoods(34));       // 34 + 89 + 19 = 142
+console.log(getSumOfHoods(34, 3));    // 34 + 3 + 19 = 56
+console.log(getSumOfHoods(34, 3, 4)); // 34 + 3 + 4 = 41
