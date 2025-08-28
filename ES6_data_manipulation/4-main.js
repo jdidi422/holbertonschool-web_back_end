19c9d0855652b847c1f14cb3d79445e89c2a4f45
jdidi422 <rassiljdidi3@gmail.com>
2025-08-28 18:32:14 +0200

    chnage

--- ES6_data_manipulation/4-main.js
+++ ES6_data_manipulation/4-main.js
@@ -3,4 +3,13 @@ import updateStudentGradeByCity from "./4-update_grade_by_city.js";
 
 console.log(updateStudentGradeByCity(getListStudents(), "San Francisco", [{ studentId: 5, grade: 97 }, { studentId: 1, grade: 86 }]));
 
-console.log(updateStudentGradeByCity(getListStudents(), "San Francisco", [{ studentId: 5, grade: 97 }]));
+console.log(updateStudentGradeByCity(getListStudents(), "San Francisco", [{ studentId: 5, grade: 97 }]));export default function createInt8TypedArray(length, position, value) {
+  if (position >= length || position < 0) {
+    throw new Error('Position outside range');
+  }
+
+  const buffer = new ArrayBuffer(length);
+  const view = new DataView(buffer);
+  view.setInt8(position, value);
+  return view;
+}
