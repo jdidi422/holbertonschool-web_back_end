bf05c20e2217d5dfda2eb9d70d2bb41da4d77fc1
jdidi422 <rassiljdidi3@gmail.com>
2025-09-06 21:47:04 +0100

    change

--- Node_JS_basic/full_server/controllers/StudentsController.js
+++ Node_JS_basic/full_server/controllers/StudentsController.js
@@ -1,29 +1,38 @@
 import { readDatabase } from '../utils.js';
 
 export default class StudentsController {
+
+  
   static async getAllStudents(req, res) {
-    const dbFile = process.argv[2]; // filename passed to server.js
+    const dbFile = process.argv[2]; 
     try {
       const data = await readDatabase(dbFile);
-      let output = 'This is the list of our students\n';
+
       const fields = Object.keys(data).sort((a, b) => a.toLowerCase().localeCompare(b.toLowerCase()));
+
+      let output = 'This is the list of our students\n';
 
       for (const field of fields) {
         output += `Number of students in ${field}: ${data[field].length}. List: ${data[field].join(', ')}\n`;
       }
 
-      res.status(200).send(output.trim());
+   
+      res.status(200).send(output);
     } catch (err) {
       res.status(500).send('Cannot load the database');
     }
   }
 
+ 
   static async getAllStudentsByMajor(req, res) {
     const dbFile = process.argv[2];
     const major = req.params.major;
+
+    
     if (!['CS', 'SWE'].includes(major)) {
       return res.status(500).send('Major parameter must be CS or SWE');
     }
+
     try {
       const data = await readDatabase(dbFile);
       res.status(200).send(`List: ${data[major].join(', ')}`);
