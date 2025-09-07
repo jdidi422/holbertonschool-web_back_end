30091cf8be0df87626651a10a81a236cabcf447c
jdidi422 <rassiljdidi3@gmail.com>
2025-09-07 03:42:10 +0100

    change

--- Node_JS_basic/full_server/controllers/AppController.js
+++ Node_JS_basic/full_server/controllers/AppController.js
@@ -1,16 +1,8 @@
 
-static getAllStudentsByMajor(req, res) {
-  const { major } = req.params;
-  if (major !== 'CS' && major !== 'SWE') {
-    return res.status(500).send('Major parameter must be CS or SWE');
+class AppController {
+  static getHomepage(req, res) {
+    res.status(200).send('Hello Holberton School!');
   }
-
-  readDatabase(process.argv[2])
-    .then((students) => {
-      const list = students[major] || [];
-      res.status(200).send(List: ${list.join(', ')});
-    })
-    .catch(() => {
-      res.status(500).send('Cannot load the database');
-    });
 }
+
+module.exports = AppController;
